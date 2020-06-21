36557d704c6054c640c1f82b4496e1589776907d
lmperez <luismanuelp1992@gmail.com>
2020-06-21 18:51:07 -0400

    chore: save inussed files

--- src/schema.ts
+++
@@ -1,1 +0,0 @@
-import { nexusPrismaPlugin } from 'nexus-prisma'
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/*
 import { GraphQLServer } from 'graphql-yoga'
 import { schema } from './schema'
 import { createContext } from './context'
@@ -7,3 +8,4 @@ new GraphQLServer({ schema, context: createContext }).start(() =>
     `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql#using-the-graphql-api`,
   ),
 )
+*/
