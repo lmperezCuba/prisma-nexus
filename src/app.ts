34e9944033876e19be298d773af7904501c27e44
lmperez <luismanuelp1992@gmail.com>
2020-06-22 08:27:03 -0400

    feat: add rules to the app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import { use } from 'nexus'
+import { use, schema } from 'nexus'
 import { prisma } from 'nexus-plugin-prisma'
 import { PrismaClient } from '@prisma/client'
+import { extractUser } from './helpers/user'
+import { permissions } from './security/rules'
 
 use(prisma())
 
 const _prisma = new PrismaClient({
-  log: ['query'],
+  // log: ['query'],
 })
+
+schema.addToContext((req) => {
+  return {
+    user: extractUser(req)
+  };
+});
+
+// Security
+use(permissions)
